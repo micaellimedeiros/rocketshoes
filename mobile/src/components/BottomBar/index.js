6c9cb997ea94575b132d5a6c7122172a1d0da197
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-03-11 02:06:17 -0300

    bottom bar files

---
+++ mobile/src/components/BottomBar/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { darken } from 'polished';
+
+import {
+  Container,
+  EmptyCart,
+  EmptyCartMessage,
+  Wrapper,
+  TotalWrapper,
+  TotalLabel,
+  TotalValue,
+  Button,
+  ButtonText,
+} from './styles';
+
+export default function BottomBar({
+  empty,
+  totalValue,
+  buttonTitle,
+  buttonIcon,
+  onPress,
+}) {
+  if (empty) {
+    return (
+      <Container>
+        <EmptyCart>
+          <Icon
+            name="remove-shopping-cart"
+            color={darken(0.27, '#7159c1')}
+            size={36}
+          />
+          <EmptyCartMessage>O carrinho está vazio :(</EmptyCartMessage>
+        </EmptyCart>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Wrapper>
+        <TotalWrapper>
+          <TotalLabel>TOTAL</TotalLabel>
+          <TotalValue>{totalValue}</TotalValue>
+        </TotalWrapper>
+        <Button onPress={onPress}>
+          <ButtonText>{buttonTitle}</ButtonText>
+          <Icon name={buttonIcon} color="#7159c1" size={18} />
+        </Button>
+      </Wrapper>
+    </Container>
+  );
+}
