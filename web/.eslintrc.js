b8b70d9fb5b2b7560c8dcacae816e38032dbad15
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-03-11 01:23:06 -0300

    eslint file

---
+++ web/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'], }
+    ],
+    'import/prefer-default-export': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/state-in-constructor': 'off',
+    'no-console': ["error", {allow: ["tron"] }],
+    'no-param-reassign': 'off',
+    'react/prop-types': 0
+  },
+};
